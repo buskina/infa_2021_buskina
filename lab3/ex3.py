d519b88b98cbae89669f52ecd1178809f1391cf7
buskina <buskina.av@phystech.edu>
2021-10-01 10:06:32 +0300

    Update ex3.py

--- lab3/ex3.py
+++ lab3/ex3.py
@@ -10,15 +10,22 @@ screen = pygame.display.set_mode((450, 650))
 
 def clo(n):
     for i in range(n):
+        "если будет скучно, можешь придумать способ размыть облака как в оригинальной картинке (17)"
         ellipse(clouds, (255,255,255,randint(10,120)), (randint(-100,350), randint(-100, 275), randint(100,500), randint(10,30)))
         ellipse(clouds, (37,37,37,randint(10,150)), (randint(-100,350), randint(-100, 275), randint(100,500), randint(10,30)))
 
 def spaceship(k,x,y):
+    "k задает масштаб фигуры. У меня есть предположение, что если выставить к=10 и отмасштабировать целую область, то фигуры"
+    "перестанут быть ступенчатыми. Проверь это, пожалуйста. По замыслу x и y являются координатами левого верхнего угла области"
+    "или ее центра. Было бы круто прояснить и этот момент. Параметр p в следующей функции отвечает за отражение всей области с"
+    "картинкой, см. конец функции"
     spsh = pygame.Surface((100*k, 100*k), pygame.SRCALPHA)
+    #Форма космического корабля
     polygon(spsh, (200, 200, 200, 150), [(10*k,90*k), (50*k,20*k),
                                (90*k,90*k), (10*k,90*k)])
     ellipse(spsh, (100,100,100), (10*k,0,90*k,30*k))
     ellipse(spsh, (200,200,200), (20*k,0,70*k,20*k))
+    #Точки на обшивке
     ellipse(spsh, (255,255,255), (15*k,13*k,8*k,6*k))
     ellipse(spsh, (255,255,255), (28*k,19*k,8*k,6*k))
     ellipse(spsh, (255,255,255), (44*k,22*k,8*k,6*k))
@@ -29,7 +36,7 @@ def spaceship(k,x,y):
 
 def alien(p,k,x,y):
     alien = pygame.Surface((100*k, 200*k), pygame.SRCALPHA)
-    #Туловище и ноги
+    #Туловище и ноги, одинаковые фигуры рисуются последовательно
     ellipse(alien, (255,255,200), (24*k,95*k,25*k,65*k))
     ellipse(alien, (255,255,200), (15*k,136*k,16*k,25*k))
     ellipse(alien, (255,255,200), (40*k,145*k,16*k,25*k))
@@ -49,6 +56,7 @@ def alien(p,k,x,y):
     ellipse(alien, (255,255,200), (5*k,47*k,10*k,7*k))
     ellipse(alien, (255,255,200), (10*k,53*k,8*k,9*k))
     ellipse(alien, (255,255,200), (15*k,60*k,5*k,9*k))
+    #Второй рог
     ellipse(alien, (255,255,200), (81*k,45*k,12*k,15*k))
     ellipse(alien, (255,255,200), (73*k,45*k,7*k,6*k))
     ellipse(alien, (255,255,200), (63*k,48*k,8*k,8*k))
@@ -61,6 +69,7 @@ def alien(p,k,x,y):
     ellipse(alien, (255,255,200), (20*k,75*k,40*k,15*k))
     ellipse(alien, (255,255,200), (25*k,80*k,30*k,15*k))
     circle(alien, (255,255,200), (40*k,90*k), 10*k)
+    #Глаза
     circle(alien, (0,0,0), (30*k,76*k), 7*k)
     circle(alien, (0,0,0), (50*k,77*k), 6*k)
     circle(alien, (255,255,255), (33*k,79*k), 2*k)
@@ -75,12 +84,14 @@ def alien(p,k,x,y):
         flip = pygame.transform.flip(alien, True, False,)
         screen.blit(flip, (x,y))
 
+#Фон
 rect(screen, (34, 43, 0), (0, 325, 450, 325))
 rect(screen, (0, 34, 43), (0, 0, 450, 325))
 circle(screen, (240,240,230), (300, 150), 60)
 clouds = pygame.Surface((450, 650), pygame.SRCALPHA)
 clo(20)
 screen.blit(clouds, (0,0))
+#Объекты
 spaceship(1,300,280)
 alien(1,0.4,100,280)
 spaceship(2,0,225)
